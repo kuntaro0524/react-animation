3778e1b9e8fe6adb0e1594960f19ccf629af0c10
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2022-05-16 09:59:47 +0900

    クリックしたら箱が表示される状態 にした

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import styled from "styled-components";
 import { useSpring, animated } from "@react-spring/web";
 
 export default function App() {
+  const [flipped, setFlipped] = useState(false);
+
+  const styles = useSpring({
+    opacity: flipped ? 1 : 0
+  });
   return (
-    <div className="App">
-      <Card />
-      <BlueCard />
+    <div className="App" onClick={() => setFlipped(!flipped)}>
+      <Card style={styles} />
+      <BlueCard style={styles} />
     </div>
   );
 }
 
-const Card = styled("div")({
+const Card = styled(animated.div)({
   position: "absolute",
   width: 500,
   height: 300,
   backgroundColor: "pink"
 });
 
-const BlueCard = styled("div")({
+const BlueCard = styled(animated.div)({
   position: "absolute",
   width: 500,
   height: 300,
