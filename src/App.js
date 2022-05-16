36da1afc9189b000e91d6e53e9dfa9ec56996f84
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2022-05-16 09:51:18 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from "react";
 import "./styles.css";
+import styled from "styled-components";
 import { useSpring, animated } from "@react-spring/web";
 
 export default function App() {
-  const styles = useSpring({
-    from: {
-      opacity: 0,
-      color: "pink",
-      y: "20px",
-      scale: "0.5"
-    },
-    to: {
-      opacity: 1,
-      color: "blue",
-      y: "100px",
-      scale: "1.0"
-    }
-  });
-
   return (
     <div className="App">
       <animated.h1 style={styles}>Hello CodeSandbox</animated.h1>
     </div>
   );
 }
+
+const Card = styled("div")({
+  width: 500,
+  height: 300,
+  backgroundColor: "pink"
+});
